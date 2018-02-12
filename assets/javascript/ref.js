2fba9255a1948ee4d6f35691ea66f7146406cf3d
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-02-11 21:42:49 -0700

    last save

---
+++ assets/javascript/ref.js
@@ -0,0 +1,44 @@
+
+//Week 3 HW assignment easy
+//Reference material
+NOOOO***document.write("")="Never use this it will erase your everything"
+
+parseInt()
+alert()
+confirm()
+prompt()
+boolean()
+console.log()
+if("condition"){}
+else{}
+arr[index]
+arr.length
+arr[index][index]
+.indexOf()
+.toLowerCase()
+
+for(i=0;i<arr.length;i++/i--){
+    someaction;
+}
+if (arr[index].charAt()==="letter" || arr[index].charAt()==="letter"){
+    someaction;
+}
+
+var userText = document.getElementById("insertText") references <span id="insertText"></span>
+
+
+
+document.onkeyup = function(event="js action"){
+    userText.textContent = event.key("the variable that was declared is going tobe set to a js action that executes when a certain key is pressed")
+};
+
+document.querySelector with innerHTML || document.getElementById with textContent
+
+var randomobj = {
+    realname:"Jehanne la Pucelle",
+    dob:"Feb 12",
+    genre:"heroess"
+    fact:"Drawn and quartered"
+}"Object consist of key:value pairs"
+
+
