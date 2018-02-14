07b06818b542e1b6bfd864293481cfb8b7a9d55b
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-02-13 17:13:50 -0700

    ''

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,11 @@
 //Week 3 HW assignment Basic
 //Author: kourtney.reynolds@gmail.com
 
+alert("start program")
 //List of letter for the computer or user to guess from
 var letters = "abcdefghijklmnopqrstuvwxyz"
-console.log(letters)
+// console.log(letters)
+
 //Total Number of Guesses before loosing
 var totalGuess = 9
 //Container for bad player guesses
@@ -13,49 +15,60 @@ var wins = 0
 var losses = 0
 var messageWin = "You win"
 var messageLose = "You lose"
+alert(letters, totalGuess,badGuesses,wins,losses,messageWin,messageWin)
 
-function playgame()
-{
+// function playGame()
+// {
+
         //While the player has not run out of guesses and niether player has a score of 10: game is in play mode
-    while (totalGuess > 0 && wins <=10 || losses<=10)
-    {
+    alert("Game is starting")
+
+    // while (totalGuess > 0 && wins <=10 || losses<=10)
+    // {
         //Start Game with computer executing guessing a letter
-        var computerGuess = letters[Math.floor(Math.random() * letters.length)]
+    var computerGuess = letters[Math.floor(Math.random() * letters.length)]
 
-        //getting a key from the user to compare to game guess
-        document.onkeyup = function(event){
-            event.key;
+    alert(computerGuess)
 
-        //Player guess
-        var playerGuess = event.key
+    //getting a key from the user to compare to game guess
+    document.onkeyup = function(event){
+        event.key;
+    }
+    //Player guess
+    var playerGuess = event.key
 
-        //Player does not run out of guesses and the user guesses the correct letter
-            if( computerGuess===playerGuess){
-                wins++;
-                console.log(wins); 
-                if(wins===10) {
-                    alert(messageWin);
-                }
-            }
-            
-        //Player guesses the wrong letter
-            else{
-                losses++;
-                console.log(losses);
-                badGuesses.push(playerGuess);
-                totalGuess--;
-            }
+    alert(playerGuess)
 
-
-
+//Player does not run out of guesses and the user guesses the correct letter
+    if( computerGuess===playerGuess){
+        wins++;
+        console.log(wins); 
+        if(wins===10) {
+            alert(messageWin);
         }
-
+        document.querySelector("showWins").innerHTML = wins;
+    }
+    
+//Player guesses the wrong letter
+    else{
+        losses++;
+        console.log(losses);
+        badGuesses.push(playerGuess);
+        totalGuess--;
+        document.querySelector("showLosses").innerHTML = losses;
     }
 
-    console.log(messageLose + " " +", the letter was" + computerGuess)
 
-}
 
+    
+
+// }
+//Log the loosing message to the console and then tell the player the letter.
+console.log(messageLose + " " +", the letter was" + computerGuess)
+
+// }
+
+// playGame()
 
 
 
